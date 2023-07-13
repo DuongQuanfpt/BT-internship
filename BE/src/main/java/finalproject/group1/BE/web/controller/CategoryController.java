b88638d4f44d4ba61520cd12196f1aa75e4cbf9a
DuongQuan <duongvuvietquan2912@gmail.com>
2023-07-13 16:39:33 +0700

    update response of category controller

--- BE/src/main/java/finalproject/group1/BE/web/controller/CategoryController.java
+++ BE/src/main/java/finalproject/group1/BE/web/controller/CategoryController.java
@@ -4,8 +4,10 @@ import finalproject.group1.BE.domain.entities.Category;
 import finalproject.group1.BE.domain.services.CategoryService;
 import finalproject.group1.BE.web.dto.request.Category.CreateCategoryRequest;
 import finalproject.group1.BE.web.dto.response.Category.CategoryListResponse;
+import finalproject.group1.BE.web.dto.response.ResponseDto;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -20,8 +22,9 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping()
-    public ResponseEntity<List<CategoryListResponse>> getAllCategories() {
-        return ResponseEntity.ok().body(categoryService.getAllCategories());
+    public ResponseEntity getAllCategories() {
+        List<CategoryListResponse> response = categoryService.getAllCategories();
+        return ResponseEntity.ok().body(ResponseDto.success(response));
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
@@ -29,7 +32,7 @@ public class CategoryController {
             MediaType.MULTIPART_FORM_DATA_VALUE })
     public ResponseEntity addCategory(@Valid @ModelAttribute CreateCategoryRequest request) {
         categoryService.createCategory(request, new Category());
-        return ResponseEntity.ok().body("");
+        return ResponseEntity.ok().body(ResponseDto.build().withMessage("OK").withHttpStatus(HttpStatus.OK));
     }
 
 }
