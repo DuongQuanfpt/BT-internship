fe2aaa75115aa74359fb4db3ffeaf1faa730b004
Tran Dieu Thao <trandieuthaoo@gmail.com>
2023-07-18 01:33:25 +0700

    Get district list by cityId

--- BE/src/main/java/finalproject/group1/BE/domain/repository/DistrictRepository.java
+++ BE/src/main/java/finalproject/group1/BE/domain/repository/DistrictRepository.java
@@ -4,9 +4,12 @@ import finalproject.group1.BE.domain.entities.District;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DistrictRepository extends JpaRepository<District,Integer> {
     Optional<District> findByIdAndCityId(int id , int cityId);
+
+    List<District> findByCityId(int cityId);
 }
